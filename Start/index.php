9bed3061e3fe9982df2186e517cf633502266bdc
yunusdemirkaya <yunusdmrk@gmail.com>
2022-11-27 02:08:35 +0300

    Form Style

--- Start/index.php
+++ Start/index.php
@@ -5,7 +5,9 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+
+    <link rel="stylesheet" href="style.css">
+    <title>Start Code - Form</title>
 </head>
 
 <form action="">
@@ -14,8 +16,33 @@
 </form>
 
 
+
 <body>
+
+
+    <section>
+
     
+        <div class="container">        
+
+            <form action="function.php" method="POST">
+            <h1>Form</h1>
+                <label for="name">Adınız</label>
+                    <input type="text" name="name" id="name" placeholder="Adınız">
+
+                <label for="sur_name">Soyadınız</label>
+                    <input type="text" name="sur_name" id="sur_name" placeholder="Soyadınız">
+
+                <label for="mail">E-Posta</label>
+                    <input type="mail" name="mail" id="mail" placeholder="E-Posta">
+
+                
+                <input type="button" value="Gönder">
+            </form>
+        </div>
+
+    </section>
+
 
 
 
