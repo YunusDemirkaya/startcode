873bbec674dc1b3578393fc750987338f0667f92
yunusdemirkaya <yunusdmrk@gmail.com>
2022-12-03 01:48:39 +0300

    Project Pdo Start

---
+++ config.php
@@ -0,0 +1,45 @@
+<?php 
+
+// $servername = "localhost";
+// $database = "veritabaniadi";
+// $username = "root";
+// $password = "";
+
+
+// //Bağlantı oluşturma
+
+// $db = mysqli_connect($servername, $username, $kullaniciadi, $sifre);
+
+// //Bağlantı Kontrolü
+
+// if(!$db) {
+
+//     die("Başarısız Bağlantı: " . mysqli_connect_error());
+// }
+
+// echo "Bağlantı Başarılı";
+// mysqli_close($db);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
